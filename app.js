95cbf5d03257fabeca219c350ef68aae58b9a9e2
ishika naikele <ishikanaikele@gmail.com>
2021-01-08 15:55:30 +0530

    Add gitignore, Procfile and update ports

--- app.js
+++ app.js
@@ -198,12 +198,10 @@ app.get("/about", function(req, res){
   res.render("about");
 });
 
-
 let port = process.env.PORT;
-if(port == null || port == ""){
+if (port == null || port == "") {
   port = 3000;
 }
-app.listen(port);
 
 
 
